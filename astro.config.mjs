1725f84032566ec0304e8704bc7ffdc66357a526
Steve Gula <github@stevegula.net>
2026-02-14 08:10:17 -0500

    Adding in SEO and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gitsorted.app',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
